22aa116db7fd90725b38096999c77a5e9e3a11d1
synbhwl <sayanbhowalemail@gmail.com>
2025-09-09 11:01:46 -0700

    refactored it into portfolio

--- blog.js
+++
@@ -1,9 +0,0 @@
-/*menu*/ 
-const menubtn = document.querySelector(".menu-btn");
-const menu = document.querySelector(".menu");
-const whole = document.querySelector(".whole");
-menubtn.addEventListener("click", menuOpen);
-function menuOpen(){
-  menu.classList.toggle("open");
-  whole.classList.toggle("open");
-}
